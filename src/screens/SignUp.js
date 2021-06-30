6b3c5b0f050332d675d1ebd4554271c1fbb4b336
2haesoo <gotn676@naver.com>
2021-06-30 00:08:39 -0400

    Add Create Account function

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,5 +1,8 @@
+import { gql, useMutation } from "@apollo/client";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 import AuthLayout from "../components/auth/AuthLayout";
 import BottomBox from "../components/auth/BottomBox";
@@ -11,41 +14,120 @@ import { FatLink } from "../components/shared";
 import routes from "../routes";
 
 const HeaderContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const Subtitle = styled(FatLink)`
-    font-size: 16px;
-    text-align: center;
-    margin-top: 10px;
+  font-size: 16px;
+  text-align: center;
+  margin-top: 10px;
+`;
+
+const CREATE_ACCOUNT_MUTATION = gql`
+  mutation createAccount(
+    $firstName: String!
+    $lastName: String
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    createAccount(
+      firstName: $firstName
+      lastName: $lastName
+      username: $username
+      email: $email
+      password: $password
+    ) {
+      ok
+      error
+    }
+  }
 `;
 
 function SignUp() {
+  const history = useHistory();
+  const onCompleted = (data) => {
+    const { createAccount: { ok, error } } = data;
+    if(!ok){
+      return;
+    }
+    history.push(routes.home);
+  }
+  const [createAccount, { loading }] = useMutation(CREATE_ACCOUNT_MUTATION, {
+    onCompleted,
+  });
+  const { register, handleSubmit, errors, formState } = useForm({
+    mode: "onChange",
+  });
+  const onSubmitValid = (data) => {
+    if (loading) {
+      return;
+    }
+    createAccount({
+      variables: {
+        ...data,
+      },
+    });
+  };
   return (
     <AuthLayout>
-        <PageTitle title="Sign Up"/>
+      <PageTitle title="Sign Up" />
       <FormBox>
         <HeaderContainer>
           <FontAwesomeIcon icon={faInstagram} size="3x" />
           <Subtitle>
-              Sign up to see photos and videos from your friends.
+            Sign up to see photos and videos from your friends.
           </Subtitle>
         </HeaderContainer>
-        <form>
-          <Input type="text" placeholder="Email" />
-          <Input type="text" placeholder="Name" />
-          <Input type="text" placeholder="Username" />
-          <Input type="password" placeholder="Password" />
-          <Button type="submit" value="Sign up" />
+        <form onSubmit={handleSubmit(onSubmitValid)}>
+          <Input
+            ref={register({
+              required: "First Name is required.",
+            })}
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+          />
+          <Input
+            ref={register}
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+          />
+          <Input
+            ref={register({
+              required: "Email is required.",
+            })}
+            type="text"
+            name="email"
+            placeholder="Email"
+          />
+          <Input
+            ref={register({
+              required: "Username is required.",
+            })}
+            type="text"
+            name="username"
+            placeholder="Username"
+          />
+          <Input
+            ref={register({
+              required: "Password is required.",
+            })}
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+          <Button
+            type="submit"
+            value={loading ? "Loading..." : "Sign Up"}
+            disabled={!formState.isValid || loading}
+          />
         </form>
       </FormBox>
-      <BottomBox
-        cta="Have an account?"
-        link={routes.home}
-        linkText="Log in"
-      />
+      <BottomBox cta="Have an account?" link={routes.home} linkText="Log in" />
     </AuthLayout>
   );
 }
