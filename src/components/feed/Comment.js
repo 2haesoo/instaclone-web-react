5c3c7f9267ec7ada0acbc2842b68b2fed8cd3e03
2haesoo <gotn676@naver.com>
2021-07-05 17:05:23 -0400

    Parsing Hashtags

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,17 +1,40 @@
+import sanitizeHtml from "sanitize-html";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { FatText } from "../shared";
+import { Link } from "react-router-dom";
+import React from "react";
 
 const CommentContainer = styled.div``;
 const CommentCaption = styled.span`
   margin-left: 10px;
+  a {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 function Comment({ author, payload }) {
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      <CommentCaption>{payload}</CommentCaption>
+      <CommentCaption>
+        {payload.split(" ").map((word, index) =>
+          /#[\w]+/.test(word) ? (
+            <React.Fragment key={index}>
+              <Link to={`/hashtags/${word}`}>
+                {word}
+              </Link>{" "}
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={index}>{word} </React.Fragment>
+          )
+        )}
+      </CommentCaption>
     </CommentContainer>
   );
 }
