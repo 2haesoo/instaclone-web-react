27ddcaab4287a48bdc3600167ef6931bd1ea5d76
2haesoo <gotn676@naver.com>
2021-07-05 18:33:10 -0400

    Finished Comment

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,11 +1,36 @@
-import sanitizeHtml from "sanitize-html";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { FatText } from "../shared";
 import { Link } from "react-router-dom";
 import React from "react";
+import { gql, useMutation } from "@apollo/client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-const CommentContainer = styled.div``;
+const DELETE_COMMENT_MUTATION = gql`
+  mutation deleteComment($id: Int!) {
+    deleteComment(id: $id) {
+      ok
+    }
+  }
+`;
+
+const SCommentDeleteBtn = styled.button`
+  border: none;
+  color: ${(props) => props.theme.accent};
+  background-color: inherit;
+  cursor: pointer;
+  &:hover {
+    color: red;
+    transform: rotate(10deg);
+  }
+`;
+
+const CommentContainer = styled.div`
+  margin-bottom: 7px;
+  display: flex;
+  align-items: center;
+`;
 const CommentCaption = styled.span`
   margin-left: 10px;
   a {
@@ -18,7 +43,34 @@ const CommentCaption = styled.span`
   }
 `;
 
-function Comment({ author, payload }) {
+function Comment({ id, isMine, author, payload, photoId }) {
+  const updateDeleteComment = (cache, result) => {
+    const {
+      data: {
+        deleteComment: { ok },
+      },
+    } = result;
+    if (ok) {
+      cache.evict({ id: `Comment:${id}` });
+      cache.modify({
+        id: `Photo:${photoId}`,
+        fields: {
+          commentNumber(prev) {
+            return prev - 1;
+          },
+        },
+      });
+    }
+  };
+  const [deleteCommentMutation] = useMutation(DELETE_COMMENT_MUTATION, {
+    variables: {
+      id,
+    },
+    update: updateDeleteComment,
+  });
+  const onDeleteClick = () => {
+    deleteCommentMutation();
+  };
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
@@ -26,20 +78,26 @@ function Comment({ author, payload }) {
         {payload.split(" ").map((word, index) =>
           /#[\w]+/.test(word) ? (
             <React.Fragment key={index}>
-              <Link to={`/hashtags/${word}`}>
-                {word}
-              </Link>{" "}
+              <Link to={`/hashtags/${word}`}>{word}</Link>{" "}
             </React.Fragment>
           ) : (
             <React.Fragment key={index}>{word} </React.Fragment>
           )
         )}
       </CommentCaption>
+      {isMine ? (
+        <SCommentDeleteBtn onClick={onDeleteClick}>
+          <FontAwesomeIcon icon={faTrash} />
+        </SCommentDeleteBtn>
+      ) : null}
     </CommentContainer>
   );
 }
 
 Comment.propTypes = {
+  id: PropTypes.number,
+  isMine: PropTypes.bool,
+  photoId: PropTypes.number,
   author: PropTypes.string.isRequired,
   payload: PropTypes.string.isRequired,
 };
